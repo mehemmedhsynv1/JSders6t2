18051c6c07df63d10217cf8fa0dd148c5b908b11
mehemmedhsynv1 <mhmmdhsynv2002@gmail.com>
2024-09-07 08:56:26 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+let month = prompt("ayin nomresini yazin");
+switch(month){
+    case "1":
+    case "01":
+        console.log("Jan");
+        break;
+    case "2":
+    case "02":
+        console.log("Feb");
+        break;
+    case "3":
+    case "03":
+        console.log("March");
+        break;
+    case "4":
+    case "04":
+        console.log("Aprel");
+        break;
+    case "5":
+    case "05":
+        console.log("May");
+        break;
+    case "6":
+    case "06":
+        console.log("June");
+        break;
+    case "7":
+    case "07":
+        console.log("July");
+        break;
+    case "8":
+    case "08":
+        console.log("Auq");
+        break;
+    case "9":
+    case "09":
+        console.log("Sept");
+        break;
+    case "10":
+        console.log("Okt");
+        break;
+    case "11":
+        console.log("Nov");
+        break;
+    case "12":
+        console.log("Dec");
+        break;
+    default:
+        alert("deyer sehvdir");             
+}
